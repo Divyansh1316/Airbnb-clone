0278d4d754ee6b29745d1ccfc6f951fe7b16bc58
Divyansh <divyanshdec@gmail.com>
2024-07-07 23:04:47 +0530

    new build

---
+++ app/search/page.js
@@ -0,0 +1,172 @@
+"use client";
+
+import React from "react";
+import Header from "../../components/Header";
+import Footer from "@/components/Footer";
+import { useSearchParams } from "next/navigation";
+import { format } from "date-fns";
+import InfoCard from "@/components/InfoCard";
+
+function Page() {
+  // const searchResults = await fetch("https://www.jsonkeeper.com/b/5NPS").then(
+  //   (res) => res.json()
+  // );
+  // console.log(searchResults);
+
+  const searchResults = [
+    {
+      img: "https://links.papareact.com/xqj",
+      location: "Private room in center of",
+      title: "Stay at this spacious Edwardian House",
+      description:
+        "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
+      star: 4.73,
+      price: "£30 / night",
+      total: "£117 total",
+      long: -0.0022275,
+      lat: 51.5421655,
+    },
+    {
+      img: "https://rb.gy/k9dfn5",
+      location: "Private room in center of",
+      title: "Independant luxury studio apartment",
+      description:
+        "2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen",
+      star: 4.3,
+      price: "£40 / night",
+      total: "£157 total",
+      long: -0.095091,
+      lat: 51.48695,
+    },
+    {
+      img: "https://links.papareact.com/uz7",
+      location: "Private room in center of",
+      title: "Studio Apartments",
+      description:
+        "4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine",
+      star: 3.8,
+      price: "£35 / night",
+      total: "£207 total",
+      long: -0.135638,
+      lat: 51.521916,
+    },
+    {
+      img: "https://links.papareact.com/6as",
+      location: "Private room in center of",
+      title: "30 mins to Oxford Street, Excel India",
+      description:
+        "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
+      star: 4.1,
+      price: "£55 / night",
+      total: "£320 total",
+      long: -0.069961,
+      lat: 51.472618,
+    },
+    {
+      img: "https://links.papareact.com/xhc",
+      location: "Private room in center of",
+      title: "Spacious Peaceful Modern Bedroom",
+      description:
+        "3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Free parking · Dry Cleaning",
+      star: 5,
+      price: "£60 / night",
+      total: "£450 total",
+      long: -0.08472,
+      lat: 51.510794,
+    },
+    {
+      img: "https://links.papareact.com/pro",
+      location: "Private room in center of",
+      title: "The Retro Room",
+      description:
+        "2 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Washing Machine",
+      star: 4.23,
+      price: "£65 / night",
+      total: "£480 total",
+      long: -0.094116,
+      lat: 51.51401,
+    },
+    {
+      img: "https://links.papareact.com/8w2",
+      location: "Private room in center of",
+      title: "5 Star Luxury Apartment",
+      description:
+        "3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
+      star: 3.85,
+      price: "£90 / night",
+      total: "£650 total",
+      long: -0.109889,
+      lat: 51.521245,
+    },
+  ];
+
+  const searchParams = useSearchParams();
+
+  // Get the query parameters
+  const location = searchParams.get("location");
+  const startDate = searchParams.get("startDate");
+  const endDate = searchParams.get("endDate");
+  const noOfGuests = searchParams.get("noOfGuests");
+
+  const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+  const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
+
+  return (
+    <div>
+      <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
+
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            300+ Stays - {range} - for {noOfGuests} guests
+          </p>
+
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
+
+          <div className="hidden lg:inline-flex mb-5 space-x-2 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Type of Place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More filters</p>
+          </div>
+
+          <div className="flex flex-col">
+            {searchResults.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
+        </section>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Page;
+
+// export async function getServerSideProps() {
+//   const searchResults = await fetch("https://links.papareact.com/isz").then(
+//     (res) => res.json()
+//   );
+//   return {
+//     props: {
+//       searchResults: searchResults,
+//     },
+//   };
+// }
--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/legacy/image";
 import {
   GlobeAltIcon,
@@ -7,13 +9,66 @@ import {
   UsersIcon,
   SearchIcon,
 } from "@heroicons/react/solid";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
-function header() {
+function header({ placeholder }) {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date()); //new date gives today's date
+  const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoGuests] = useState(1);
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput("");
+  };
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const search = () => {
+    const params = new URLSearchParams({
+      location: searchInput,
+      startDate: startDate.toISOString(),
+      endDate: endDate.toISOString(),
+      noOfGuests: noOfGuests.toString(),
+    });
+    router.push(`/search?${params.toString()}`);
+  };
+
+  // const search = () => {
+  //   router.push({
+  //     pathname: "/search",
+  //     query: {
+  //       location: searchInput,
+  //       startDate: startDate.toISOString(),
+  //       endDate: endDate.toISOString(),
+  //       noOfGuests,
+  //     },
+  //   });
+  // };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* sticky stics to the top, z-index tells which layer its on(its gonna bring it to front always) */}
+
       {/* left div */}
-      <div className="relative flex items-center h-10 w-32 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 w-32 cursor-pointer my-auto"
+      >
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -25,9 +80,11 @@ function header() {
       {/* middle div */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)} //get the value/text in text box
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
           type="text"
-          placeholder="Start your seach"
+          placeholder={placeholder || "Start your seach"}
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
@@ -41,6 +98,38 @@ function header() {
           <UserCircleIcon className="h-6"></UserCircleIcon>
         </div>
       </div>
+      {/* check if search box has some value and is not empty */}
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={noOfGuests}
+              onChange={(e) => setNoGuests(e.target.value)}
+              type="number"
+              min={1}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow text-grey-500">
+              Cancel
+            </button>
+            <button onClick={search} className="flex-grow text-red-400">
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }

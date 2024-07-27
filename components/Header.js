739ea8af3aad44afe5b99f4bd6cf55a008f39880
Divyansh Dhaundiyal <112102507+Divyansh1316@users.noreply.github.com>
2024-07-27 21:08:12 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -76,7 +76,7 @@ function Header({ placeholder }) {
       </div>
 
       {/* middle div */}
-      <div className="hidden sm475:flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)} //get the value/text in text box
